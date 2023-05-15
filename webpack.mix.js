35c2783173859308922dd431017a44317bd2463d
Guilherme Bruenning <bruenning.guilherme@gmail.com>
2023-05-15 13:19:23 -0300

    ajustes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,19 @@
 let mix = require('laravel-mix');
 
+// Path: webpack.config.js
+
+const path = require('path');
+
 mix
     .js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
 
-// Path: webpack.config.js
-
-const path = require('path');
-
-module.exports = {
+max.webpackConfig({
     resolve: {
         alias: {
             '@': path.resolve('resources/js'),
             '@sass': path.resolve('resources/sass'),
         },
     },
-};
+})
