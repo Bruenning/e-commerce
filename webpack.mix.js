7782b51ba25c4599ef2f48c25aaf9e0a77b4e091
Guilherme Bruenning <bruenning.guilherme@gmail.com>
2023-05-15 15:37:13 -0300

    bug

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,17 +3,39 @@ let mix = require('laravel-mix');
 // Path: webpack.config.js
 
 const path = require('path');
+const { rule } = require('postcss');
 
 mix
     .js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
 
-mix.webpackConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve('resources/js'),
-            '@sass': path.resolve('resources/sass'),
+mix.extend('vue', (mix) => {
+    const vueLoaderConfig = {
+        transformToRequire : {
+            video : ['src', 'poster'],
+            source : 'src',
+            img : 'src',
+            image : 'xlink:href',
         },
-    },
+    }
+
+    mix.webpackConfig({
+        resolve: {
+            extensions: ['.js', '.vue', '.json'],
+            alias: {
+                '@': path.resolve('resources/js'),
+            },
+            modules: {
+                rules: [
+                    {
+                        test: /\.vue$/,
+                        loader: 'vue-loader',
+                        options: vueLoaderConfig,
+                    },
+                ],
+            },
+        },
+    })
 })
+
