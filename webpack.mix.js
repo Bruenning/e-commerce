f929db3e6e9d9af8d82013ea583a0fd44b96bb8b
Guilherme Bruenning <bruenning.guilherme@gmail.com>
2023-06-15 09:12:57 -0300

    ajustes para testes

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,49 +2,36 @@ let mix = require('laravel-mix');
 
 // Path: webpack.config.js
 
-const path = require('path');
-const fs =  require('fs');
-
-const package = JSON.parse(fs.readFileSync('./package.json'));
-
 mix
     .js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
     .copy('resources/images', 'public/images')
-    .copy('resources/fonts', 'public/fonts')
-    .babelConfig({
-        presets: ['@babel/preset-env'],
-    })
+    // .babelConfig({
+    //     presets: ['@babel/preset-env'],
+    // })
 
 
 mix.webpackConfig({
     resolve: {
         extensions: ['.js', '.vue', '.json'],
-        alias: {
-            '@': path.resolve('resources/js'),
-        },
         fallback: {
             os: require.resolve('os-browserify/browser'),
-            https: false,
-            http: false,
-            crypto: false,
-            stream: false,
             path: require.resolve('path-browserify'),
             fs: false,
         }
     },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-            },
-        ]
-    }
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.vue$/,
+//                 loader: 'vue-loader',
+//             },
+//         ]
+    // }
     
 })
 
 mix.disableNotifications()
 
-mix.version()
+// mix.version()
