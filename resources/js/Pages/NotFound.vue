c2f44b3176c4dd8661c2f0bb2bd42315e17a3cad
Guilherme Henrique Bruenning <bruenning.guilherme@gmail.com>
2023-06-15 10:27:22 -0300

    Update NotFound.vue

--- resources/js/Pages/NotFound.vue
+++ resources/js/Pages/NotFound.vue
@@ -1,1 +1,10 @@
-
+<template>
+    <Vcontainer class="h-100">
+        <Vrow class="h-100 d-flex justify-content-center align-item-center ">
+            <Vcol cols="3">
+                <h1>404</h1>
+                <h2>Page not found</h2>
+            </Vcol>
+        </Vrow>
+    </Vcontainer>
+</template>
